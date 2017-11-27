dd25620c462acca0858ed5e875cfdabaf74d11ef
michaelbavier <michaelbavier89@gmail.com>
2017-11-27 14:35:54 +0100

    uptade

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title>LOGO</title>
+</head>
+<body>
+  <header>
+    <p class="color"></p>
+    <nav>
+      <img class="logoH" src="image/1.png" alt="">
+      <div id="searchbar">
+        <center><form action="" class="formulaire">
+          <input class="champ" type="text" placeholder="Recherche"/>
+          <input class="button header" type="button" value="GO"/>
+        </form></center>
+      </div>
+      <div class="login">
+
+        <img class="phone"src="image/icons8.jpg" alt="">
+        <p>(+000)123 345 653</p>
+        <button class="lgn" type="button" >LOGIN</button>
+      </div>
+    </nav>
+  </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer>
+<div class="block">
+  <h1>SUBSCRIBE TO US</h1>
+<input class="mail" type="text">
+<input class="button foot"type="button" value="SEND">
+</div>
+<img class="logoF"src="image/1.png" alt=""><br>
+<hr>
+<p>© 2017 One Movies All Rights reserved Design By (BeCode) </p>
+
+
+
+
+</footer>
+
+
+
+
+
+
+<script type="/js/main.js">
+
+</script>
+
+  </body>
+</html>
