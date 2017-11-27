199971d35c6b6cb8fd1b47bb5945b8f9ed901c4a
Alitunaarel <alitunaarel@gmail.com>
2017-11-27 14:52:25 +0100

    update

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>ALLEZCINE</title>
+
+  </head>
+    <body>
+      <header>
+<p class="color"></p>
+<nav>
+<img class="logoH" src="image/1.png" alt="">
+<div id="searchbar">
+<center><form action="" class="formulaire">
+<input class="champ" type="text" placeholder="Recherche"/>
+<input class="button header" type="button" value="GO"/>
+</form></center>
+</div>
+<div class="login">
+
+    <img class="phone"src="image/icons8.jpg" alt="">
+    <p>(+000)123 345 653</p>
+    <button class="lgn" type="button" >LOGIN</button>
+  </div>
+</nav>
+</header>
+
+
+
+
+
+    <div class="bignav">
+
+
+      <div class="navbar">
+
+                <div id="home" class="div1">Home</div>
+                <div class="dropdown1">
+            <button class="dropbtn1">Film
+              <i class="fa fa-caret-down1"></i>
+            </button>
+            <div class="dropdown-content1">
+              <a href="#">Actions</a>
+              <a href="#">Adventures</a>
+              <a href="#">Policier</a>
+            </div>
+          </div>
+
+                <div class="dropdown">
+            <button class="dropbtn">Series
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="#">Actions</a>
+              <a href="#">Adventures</a>
+              <a href="#">Policier</a>
+            </div>
+          </div>
+
+        <div class="div2"></div>
+        <div class="div2"></div>
+        <div class="div2"></div>
+        <div class="div2"></div>
+
+      </div>
+    </div>
+
+
+    <!-- resaux socieuax -->
+
+
+<div id="mySidenav" class="sidenav">
+  <a href="#" id="about">Facebook</a>
+  <a href="#" id="blog">Twitter</a>
+  <a href="#" id="projects">Instagram</a>
+  <a href="#" id="contact">Contact</a>
+</div>
+
+
+
+
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" href="main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="http://v4-alpha.getbootstrap.com/dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="http://v4-alpha.getbootstrap.com/assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://v4-alpha.getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+
+  </body>
+</html>
